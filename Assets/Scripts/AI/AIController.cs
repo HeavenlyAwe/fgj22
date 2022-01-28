393bb9ccb21997e171e9d37aace35367f494a530
CurlyNikolai <48209434+CurlyNikolai@users.noreply.github.com>
2022-01-29 01:08:48 +0200

    Fixed follower roaming movement. Added different meshes for different states.

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -8,6 +9,7 @@ using Random = UnityEngine.Random;
 public class AIController : MonoBehaviour
 {
 
+    // States for the follower
     public enum State
     {
         Roam,
@@ -15,25 +17,47 @@ public class AIController : MonoBehaviour
         Die,
     }
 
+    // Factions the follower can follow
+    public enum Following
+    {
+        Neutral,
+        Fire,
+        Water,
+    }
+
     [Header("States")]
     public State state;
+    public Following currentFollowing = Following.Neutral;
 
     [Header("Movement")] 
     public float followerSpeed = 1.0f;
     public float roamingRadius = 5.0f;
 
-    private NavMeshAgent navMeshAgent;
-    private Vector3 anchorPoint;
-
+    [Header("Follower Prefabs")] 
+    public GameObject neutralFollower;
+    public GameObject fireFollower;
+    public GameObject waterFollower;
+    
+    private NavMeshAgent _navMeshAgent;
+    private Vector3 _anchorPoint;
+    
     private void Awake()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        anchorPoint = transform.position;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        _anchorPoint = transform.position;
+        _navMeshAgent.speed = followerSpeed;
+        changeFollowing(currentFollowing);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        // State machine for follower
         switch (state)
         {
             case State.Roam:
@@ -46,11 +70,30 @@ public class AIController : MonoBehaviour
         }
     }
 
+    // Function for updating this followers current following
+    public void changeFollowing(Following newFollowing)
+    {
+        // This is lazy code
+        neutralFollower.SetActive(false);
+        fireFollower.SetActive(false);
+        waterFollower.SetActive(false);
+        
+        if (newFollowing==Following.Neutral) neutralFollower.SetActive(true);
+        if (newFollowing==Following.Fire) fireFollower.SetActive(true);
+        if (newFollowing==Following.Water) waterFollower.SetActive(true);
+
+        currentFollowing = newFollowing;
+
+    }
+    
     // Function for moving follower randomly within an "anchor" unit circle
     private void RandomMove()
     {
-        
-        Vector3 randomPosition = anchorPoint + (Vector3) Random.insideUnitCircle * roamingRadius;
+        // Don't generate new random position to go to, if the previous one hasn't been reached
+        if (_navMeshAgent.remainingDistance > 0) return;
+
+        Vector2 circleRandomPosition = Random.insideUnitCircle * roamingRadius;
+        Vector3 randomPosition = _anchorPoint + new Vector3(circleRandomPosition.x, 0.0f, circleRandomPosition.y);
 
         // Check for closest position to randomPosition on navmesh. If not found, return.
         NavMeshHit hit;
@@ -58,10 +101,11 @@ public class AIController : MonoBehaviour
         {
             return;
         }
-        
+
         // If a random point on the navmesh was found, move follower to that position.
-        navMeshAgent.destination = hit.position;
+        _navMeshAgent.destination = hit.position;
         
+        return;
     }
-    
+
 }
