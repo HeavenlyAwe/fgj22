f2e1c5bd4380b517b9d7c0b9c855dff02f866761
Christoffer Fridlund <christoffer.fridlund@gmail.com>
2022-01-29 01:41:35 +0200

    Jumping works now.

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,9 +13,30 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
 
+    // This allows the player to perform multiple jumps if necessary
+    public int maxNumberOfJumps = 1;
+
+    private int jumpsRemaining = 0;
+    private bool jumping = false;
+    private Vector3 move = Vector3.zero;
+
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    public void OnJump()
+    {
+        if (jumpsRemaining > 0)
+        {
+            jumping = true;
+        }
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+        move = new Vector3(inputVec.x, 0, inputVec.y);
     }
 
     void Update()
@@ -22,10 +44,11 @@ public class PlayerMovement : MonoBehaviour
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
+            jumpsRemaining = maxNumberOfJumps;
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
@@ -34,9 +57,11 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (jumping)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            jumping = false;
+            jumpsRemaining -= 1;
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
