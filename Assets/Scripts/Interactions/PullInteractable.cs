6f3e827053a434093ab61a72550e2e42199c9d18
Christoffer Fridlund <christoffer.fridlund@gmail.com>
2022-01-29 18:35:22 +0200

    Cleaning up in the interactable scripts.

--- Assets/Scripts/Interactions/PullInteractable.cs
+++ Assets/Scripts/Interactions/PullInteractable.cs
@@ -4,55 +4,75 @@ using UnityEngine;
 
 public class PullInteractable : Interactable
 {
-    public float MoveSpeed = 5.0f;
+
+    // TODO: The commented code could be useful if smoothly animating the movement...
+
+    //public float MoveSpeed = 5.0f;
 
     [SerializeField]
     private Vector3 pullDirection;
 
-    private Vector3 targetPosition;
+    //private Vector3 targetPosition;
 
-    private bool moving = false;
+    //private bool moving = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        targetPosition = transform.position;
-    }
+    //void Start()
+    //{
+    //    targetPosition = transform.position;
+    //}
 
     public override void Interact(Transform interactor)
     {
-        Pull(interactor);
-    }
+        //Pull(interactor);
 
-    public void Pull(Transform interactorTransform)
-    {
-        if (!moving)
+        //if (!moving)
+        //{
+        Vector3 pushDirection = (transform.position - interactor.transform.position);
+        if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
         {
-            Vector3 pushDirection = (transform.position - interactorTransform.transform.position);
-            if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
-            {
-                pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
-            }
-            else
-            {
-                pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
-            }
-            pullDirection = -pushDirection;
-
-            targetPosition = transform.position + pullDirection;
-            moving = true;
+            pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (moving)
+        else
         {
-            transform.position = targetPosition;
-            moving = false;
+            pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
         }
+        pullDirection = -pushDirection;
+
+        transform.position += pullDirection;
+            //moving = true;
+        //}
     }
+
+    //public void Pull(Transform interactorTransform)
+    //{
+    //    if (!moving)
+    //    {
+    //        Vector3 pushDirection = (transform.position - interactorTransform.transform.position);
+    //        if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
+    //        {
+    //            pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
+    //        }
+    //        else
+    //        {
+    //            pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
+    //        }
+    //        pullDirection = -pushDirection;
+
+    //        targetPosition = transform.position + pullDirection;
+    //        moving = true;
+    //    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (moving)
+    //    {
+    //        transform.position = targetPosition;
+    //        moving = false;
+    //    }
+    //}
 
     public override bool IsGrabbable()
     {
--- Assets/Scripts/Interactions/PushInteractable.cs
+++ Assets/Scripts/Interactions/PushInteractable.cs
@@ -4,55 +4,70 @@ using UnityEngine;
 
 public class PushInteractable : Interactable
 {
-    public float MoveSpeed = 5.0f;
+
+    // TODO: The commented code could be useful if smoothly animating the movement...
+
+    //public float MoveSpeed = 5.0f;
 
     [SerializeField]
     private Vector3 pushDirection;
 
-    private Vector3 targetPosition;
+    //private Vector3 targetPosition;
 
-    private bool moving = false;
+    //private bool moving = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        targetPosition = transform.position;
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    targetPosition = transform.position;
+    //}
 
     public override void Interact(Transform interactor)
     {
-        Debug.Log("interacting with " + gameObject.name);
-        Push(interactor);
-    }
+        //Debug.Log("interacting with " + gameObject.name);
+        //Push(interactor);
 
-    public void Push(Transform interactor)
-    {
-        if (!moving)
+        pushDirection = (transform.position - interactor.position);
+        if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
         {
-            pushDirection = (transform.position - interactor.position);
-            if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
-            {
-                pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
-            }
-            else
-            {
-                pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
-            }
-
-            targetPosition = transform.position + pushDirection;
-            moving = true;
+            pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (moving)
+        else
         {
-            transform.position = targetPosition;
-            moving = false;
+            pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
         }
+
+        transform.position += pushDirection;
     }
+
+    //public void Push(Transform interactor)
+    //{
+    //    if (!moving)
+    //    {
+    //        pushDirection = (transform.position - interactor.position);
+    //        if (Mathf.Abs(pushDirection.z) > Mathf.Abs(pushDirection.x))
+    //        {
+    //            pushDirection = new Vector3(0, 0, pushDirection.z).normalized;
+    //        }
+    //        else
+    //        {
+    //            pushDirection = new Vector3(pushDirection.x, 0, 0).normalized;
+    //        }
+
+    //        targetPosition = transform.position + pushDirection;
+    //        moving = true;
+    //    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (moving)
+    //    {
+    //        transform.position = targetPosition;
+    //        moving = false;
+    //    }
+    //}
 
     public override bool IsGrabbable()
     {
